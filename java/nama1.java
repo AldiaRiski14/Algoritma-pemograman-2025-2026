08f740a70f8bc05f80bab15812870688e203c7a3
Aldia Riski Fauzi <150106598+AldiaRiski14@users.noreply.github.com>
2025-10-13 11:25:22 +0700

    Create nama1.java

---
+++ java/nama1.java
@@ -0,0 +1,13 @@
+public class nama1 {
+    public static void main(String[] args) {
+        String nama = "Aldia";
+        int umur = 19;
+        double tinggi = 178;
+        boolean aktif = true;
+
+        System.out.println("Nama" + nama);
+        System.out.println("Umur" + umur);
+        System.out.println("Tinggi" + tinggi);
+        System.out.println("Status Aktif" + aktif);
+    }
+}
