056d593c0cbf34c70b604294800231c7ca0bac44
Aldia Riski Fauzi <150106598+AldiaRiski14@users.noreply.github.com>
2025-10-13 11:25:42 +0700

    Create nama1.cpp

---
+++ c dan c++/nama1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nama = "Aldia";
+    int umur =19;
+    float tinggi = 178;
+    bool aktif = "true";
+
+    cout <<"Nama:" <<nama << endl;
+    cout <<"Umur:" <<umur << endl;
+    cout <<"Tinggi:" <<tinggi << endl;
+    cout <<"Status Aktif:" <<aktif << endl;
+    return 0;
+}
