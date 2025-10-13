3316b37105bef79a8a903ab0b1986601bd3e9742
Aldia Riski Fauzi <150106598+AldiaRiski14@users.noreply.github.com>
2025-10-13 11:25:18 +0700

    Create nama1.php

---
+++ php/nama1.php
@@ -0,0 +1,11 @@
+<?php 
+$nama = "Aldia";
+$umur = 19;
+$tinggi = 178;
+$aktif = "true";
+
+echo "Nama: $nama<br>";
+echo "Umur: $umur<br>";
+echo "Tinggi: $tinggi<br>";
+echo "Status aktif: $aktif";
+?>
