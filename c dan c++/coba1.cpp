b1e9e86ed1c2599a1b450425e528ce7bc3574041
Aldia Riski Fauzi <150106598+AldiaRiski14@users.noreply.github.com>
2025-10-13 11:25:45 +0700

    Create coba1.cpp

---
+++ c dan c++/coba1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+main()
+{
+    int a= 7;
+    int b = 2;
+    char c[15] = "Aldia Riski";
+
+    puts("Nama ");
+    puts(c);
+    printf("Terdiri dari %d Hurup dan %d Kata ", a, b);
+}
