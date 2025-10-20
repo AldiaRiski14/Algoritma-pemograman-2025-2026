cfa8aad6f5247be01e6981ab417502c433184480
Aldia Riski Fauzi <150106598+AldiaRiski14@users.noreply.github.com>
2025-10-20 22:35:20 +0700

    Create variabel.py

---
+++ python/variabel.py
@@ -0,0 +1,13 @@
+print('---Belajar Variabel---')
+
+char ='A'
+string= "Aldia Riski Fauzi"
+a = 19
+penghasilan1 = 10000000
+nilai_uts = 89.24
+
+print(char)
+print(string)
+print(a)
+print(penghasilan1)
+print(nilai_uts)
