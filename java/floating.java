55a97c784ff167a87c5a7fc945a9d1424627b735
Aldia Riski Fauzi <150106598+AldiaRiski14@users.noreply.github.com>
2025-10-20 22:34:42 +0700

    Create floating.java

---
+++ java/floating.java
@@ -0,0 +1,9 @@
+public class floating {
+    public static void main(String[] args) {
+        float float_pi = 3.14f;
+        double long_pi = 3.141592653589793d;
+
+        System.out.println("Nilai tipe data float= " + float_pi);
+        System.out.println("Nilai tipe data double= " + long_pi);
+    }
+}
