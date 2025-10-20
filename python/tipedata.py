8ced7b0712e844d85852f0ca78eff71fe3974817
Aldia Riski Fauzi <150106598+AldiaRiski14@users.noreply.github.com>
2025-10-20 22:35:18 +0700

    Create tipedata.py

---
+++ python/tipedata.py
@@ -0,0 +1,33 @@
+from calendar import c
+
+
+print('---BELAJAR TIPE DATA---')
+
+#int
+y =3
+z =5+3
+print(z)
+
+#float num
+a =7.5
+print(a)
+
+#single quote
+myFirstName ='Aldia Riski'
+
+#double
+myLastName = 'Fauzi'
+
+myAge = '19'
+
+myHobby = """Hobi saya adalah bermain game dan Hiking"""
+
+print(myFirstName + " " + myLastName)
+print("Umur saya" + myAge)
+print("Hobby saya adalah" + myHobby)
+
+a = True
+b = False
+c = a
+
+print(c)
