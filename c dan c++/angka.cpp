52397c7f4175fccf46f53dfd19bb5b6671a5a09a
Aldia Riski Fauzi <150106598+AldiaRiski14@users.noreply.github.com>
2025-10-27 11:10:51 +0700

    Create angka.cpp

---
+++ c dan c++/angka.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "Masukkan angka pertama: ";
+    cin >> a;
+    cout << "Masukkan angka kedua: ";
+    cin >> b;
+
+    if(a > b){
+        cout << a << " lebih besar dari " << b << endl;
+    } else if(a < b){
+        cout << a << " lebih kecil dari " << b << endl;
+    } else {
+        cout << "kedua angka sama besar" << endl;
+    }
+}
